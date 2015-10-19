6cc7004a60cde3ee51713e8c02ebf1141b09e32e
Christopher Pereira <christopher.pereira8585@hotmail.com>
2015-10-18 19:16:17 -0700

    Star in search list

--- app/src/main/java/com/thetubeteam/mytube/SearchUtil.java
+++ app/src/main/java/com/thetubeteam/mytube/SearchUtil.java
@@ -69,7 +69,7 @@ public class SearchUtil {
 
             // To increase efficiency, only retrieve the fields that the
             // application uses.
-            search.setFields("items(id/kind,id/videoId,snippet/title,snippet/thumbnails/default/url)");
+            //search.setFields("items(id/kind,id/videoId,snippet/title,snippet/thumbnails/default/url)");
             search.setMaxResults(NUMBER_OF_VIDEOS_RETURNED);
 
             // Call the API and print results.
